82110e0834694b3715d8b3782e2dfcb15d6cc6fa
khihadysucahyo <khihady.ks@gmail.com>
2020-10-12 22:52:32 +0700

    init unit test

---
+++ test/env_test.go
@@ -0,0 +1,15 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+// TestEnv test
+func TestEnv(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		t.Error("Error loading .env file: ", err)
+	}
+}
