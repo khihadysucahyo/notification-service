d38769bb09ddc0190377bdf127f9ed66ce8d87f3
khihadysucahyo <khihady.ks@gmail.com>
2020-10-12 22:17:31 +0700

    modified mongo uri

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	controller "github.com/khihadysucahyo/notification-service/controllers"
 	"github.com/khihadysucahyo/notification-service/routes"
 	"github.com/khihadysucahyo/notification-service/utils"
@@ -14,16 +12,14 @@ func main() {
 	e := echo.New()
 
 	// Database connection
-	db, err := mgo.Dial(utils.GetEnv("DB_MONGO_HOST"))
+	uri := "mongodb://" + utils.GetEnv("DB_MONGO_HOST") + ":" + utils.GetEnv("DB_MONGO_PORT")
+	db, err := mgo.Dial(uri)
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
 
-	fmt.Println("HAHA", utils.GetEnv("DB_MONGO_HOST"))
-
 	// Initializedbconfig
 	c := &controller.Controller{DB: db}
-	fmt.Println(c)
 
 	// routing
 	apiGroup := e.Group("/api")
